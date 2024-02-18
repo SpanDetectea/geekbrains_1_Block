d688eff7796430adf9d7f1abb4a90b81764391e8
Span <spandetectea@gmail.com>
2024-02-18 21:40:59 +0300

    Решение задачи

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array = { "Hello", "2", "world", ":-)" };
+// string[] array = { "1234", "1567", "-2", "computer science" };
+// string[] array = { "Russia", "Denmark", "Kazan" };
+int count = 0;
+for (int i = 0; i <= array.GetLength(0) - 1; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        count++;
+    }
+}
+string[] newArray = new string[count];
+count = 0;
+for (int i = 0; i <= array.GetLength(0) - 1; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        newArray[count] = array[i];
+        count++;
+    }
+}
+System.Console.WriteLine($"[{String.Join(", ", newArray)}]");
